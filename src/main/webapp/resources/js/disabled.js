1cc502d708c436db0f7aca313b7fae6bd0dc37bb
morikodai <user@usernoMacBook-Air.local>
2021-05-27 16:55:55 +0900

    拡張機能4修正しました

--- src/main/webapp/resources/js/disabled.js
+++ src/main/webapp/resources/js/disabled.js
@@ -2,10 +2,8 @@ $(function() {
 	var status=$('#RentingStatus').val();
 	
 	if (status==='貸し出し可') {
-			//$('.btn_rentBook','.btn_deleteBook').prop('disabled',true);
            $('.btn_returnBook').prop('disabled', true);
         } else if(status==='貸し出し中') {
-            //$('.btn_returnBook').prop('disabled',true);
 			$('.btn_rentBook').prop('disabled',true);
 			$('.btn_deleteBook').prop('disabled',true);
         }
